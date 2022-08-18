a9a407342efd9e71c46e0cdb40733f272767a42a
April Lee <april210608@gmail.com>
2022-08-18 09:48:53 -0700

    hamburger menu done

--- javaScript/everyPage.js
+++ javaScript/everyPage.js
@@ -1,24 +1,41 @@
-window.onload=function(){
-  const upArrow = document.querySelector('.up_arrow');
-  upArrow.addEventListener("click", ()=> {
-    window.scrollTo(0,0);
-});
-}
+window.onload = function () {
+  const upArrow = document.querySelector(".up_arrow");
+  upArrow.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
 
+  (() => {
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("overlay");
+    let open = false;
+  
+    const change = () => {
+      if (!open) {
+        hamburger.classList.add("open");
+        menu.classList.add("menu");
+      } else {
+        hamburger.classList.remove("open");
+        menu.classList.remove("menu");
+      }
+      open = !open;
+    };
+  
+    hamburger.addEventListener("click", change);
+  })();
+};
 
-const items = document.querySelectorAll('.appear');
+const items = document.querySelectorAll(".appear");
 
-const active = function(entries){
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-        entry.target.classList.add('inview'); 
-        }else{
-            entry.target.classList.remove('inview'); 
-        }
-    });
-}
+const active = function (entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("inview");
+    } else {
+      entry.target.classList.remove("inview");
+    }
+  });
+};
 const io2 = new IntersectionObserver(active);
- for(let i=0; i < items.length; i++){
-    io2.observe(items[i]);
- }
-
+for (let i = 0; i < items.length; i++) {
+  io2.observe(items[i]);
+}
