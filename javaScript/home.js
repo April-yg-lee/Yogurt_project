790026b65385fa0c010253b40c0e07df849a1da2
April Lee <april210608@gmail.com>
2022-08-20 14:42:46 -0700

    revised loation, order now

--- javaScript/home.js
+++ javaScript/home.js
@@ -25,6 +25,10 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+
+  // slideIndex++;
+  // if (slideIndex > slides.length) {slideIndex = 1}
+  // setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
 // let slideIndex = 0;
