a7ac85d1000b21ad680698ff02ce50b3c16be54c
April Lee <april210608@gmail.com>
2022-09-01 18:15:29 -0700

    changed nav language

--- javaScript/everyPage.js
+++ javaScript/everyPage.js
@@ -14,17 +14,45 @@ i18next.on("languageChanged", () => {
 
 function settingForChangingLanguage() {
   const viewLanguage = document.querySelectorAll(".selectLanguage");
+  // const selectHome = document.querySelectorAll(".selectHome");
+  // const selectOurStory = document.querySelectorAll(".selectOurStory");
+  // const selectMenu = document.querySelectorAll(".selectMenu");
+  // const selectLocation = document.querySelectorAll(".selectLocation");
+  // const selectContact = document.querySelectorAll(".selectContact");
+  
 
   if (viewLanguage[0].textContent === "English") {
     sessionStorage.setItem('selectedLanguage', 'en');
     selectedLanguage = "en";
     viewLanguage[0].innerHTML = "Korean";
     viewLanguage[1].innerHTML = "Korean";
+    // selectHome[0].innerHTML = "Home";
+    // selectHome[1].innerHTML = "Home";
+    // selectOurStory[0].innerHTML = "Our Story";
+    // selectOurStory[1].innerHTML = "Our Story";
+    // selectMenu[0].innerHTML = "Menu";
+    // selectMenu[1].innerHTML = "Menu";
+    // selectLocation[0].innerHTML = "Location";
+    // selectLocation[1].innerHTML = "Location";
+    // selectContact[0].innerHTML = "Contact";
+    // selectContact[1].innerHTML = "Contact";
+   
   } else {
     sessionStorage.setItem('selectedLanguage', 'ko');
     selectedLanguage = "ko";
     viewLanguage[0].innerHTML = "English";
     viewLanguage[1].innerHTML = "English";
+    // selectHome[0].innerHTML = "홈";
+    // selectHome[1].innerHTML = "홈";
+    // selectOurStory[0].innerHTML = "브랜드";
+    // selectOurStory[1].innerHTML = "브랜드";
+    // selectMenu[0].innerHTML = "메뉴&주문";
+    // selectMenu[1].innerHTML = "메뉴&주문";
+    // selectLocation[0].innerHTML = "위치";
+    // selectLocation[1].innerHTML = "위치";
+    // selectContact[0].innerHTML = "문의하기";
+    // selectContact[1].innerHTML = "문의하기";
+  
 
   }
   i18next.changeLanguage(selectedLanguage);
@@ -32,12 +60,17 @@ function settingForChangingLanguage() {
 
 function settingForTextOnNavibar() {
   const viewLanguage1 = document.querySelectorAll(".selectLanguage");
+ 
   if (selectedLanguage === "en") {
     viewLanguage1[0].innerHTML = "Korean";
     viewLanguage1[1].innerHTML = "Korean";
+    
+   
   } else {
     viewLanguage1[0].innerHTML = "English";
     viewLanguage1[1].innerHTML = "English";
+
+  
   }
 }
 
